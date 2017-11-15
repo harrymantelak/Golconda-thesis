55f157e8f7500aa9668bbec45f8dd32f78a6c34d
harrymantelak <harry.mantelakis@gmail.com>
2017-11-15 13:46:45 +0200

    -Added Block number/time checks -Added Refund Functionality

--- CrowdfundedCharity.sol
+++ CrowdfundedCharity.sol
@@ -7,8 +7,9 @@ contract CrowdFunding {
         uint amount;
     }
     struct Campaign {
-        address beneficiary;
+        address beneficiary; // This will be the adress to be paid for the order
         uint fundingGoal;
+        uint fundingDeadlineBlock;
         uint numFunders;
         uint amount;
         mapping (uint => Funder) funders;
@@ -17,26 +18,49 @@ contract CrowdFunding {
     uint numCampaigns;
     mapping (uint => Campaign) campaigns;
 
-    function newCampaign(address beneficiary, uint goal) returns (uint campaignID) {
+    function newCampaign(address beneficiary, uint goal, uint deadline) public returns (uint campaignID) {
         campaignID = numCampaigns++; // campaignID is return variable
         // Creates new struct and saves in storage. We leave out the mapping type.
-        campaigns[campaignID] = Campaign(beneficiary, goal, 0, 0);
+        campaigns[campaignID] = Campaign(beneficiary, goal, deadline, 0, 0);
     }
 
-    function contribute(uint campaignID) payable {
+    function contribute(uint campaignID) public payable {
         Campaign storage c = campaigns[campaignID];
         // Creates a new temporary memory struct, initialised with the given values
         // and copies it over to storage.
         // Note that you can also use Funder(msg.sender, msg.value) to initialise.
+        require( c.fundingDeadlineBlock < block.number );
+        require( c.amount + msg.value > c.fundingGoal );
+
         c.funders[c.numFunders++] = Funder({addr: msg.sender, amount: msg.value});
         c.amount += msg.value;
     }
 
-    function checkGoalReached(uint campaignID) returns (bool reached) {
+    function checkGoalReached(uint campaignID) public returns (bool reached) {
         Campaign storage c = campaigns[campaignID];
-        if (c.amount < c.fundingGoal)
+        if ( c.fundingDeadlineBlock <= block.number) {
+          if (c.amount < c.fundingGoal) {
+            //Charity Failed, Initiate Refunds
+            for(uint i=1; i<c.numFunders; i++){
+              uint refund =c.funders[i].amount;
+              c.funders[i].amount =0;
+              c.funders[i].addr.transfer(refund);
+
+            }
+          }
+          if (c.amount >= c.fundingGoal){
+            // Charity Successful, Transfer Amount
+            uint amount = c.amount;
+            c.amount = 0;
+            c.beneficiary.transfer(amount);
+            return true;
+
+          }
+        }
+        if (c.amount < c.fundingGoal) {
             return false;
-        uint amount = c.amount;
+        }
+        amount = c.amount;
         c.amount = 0;
         c.beneficiary.transfer(amount);
         return true;
