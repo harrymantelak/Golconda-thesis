b4bcc96668f53d9441ebcc13da5ba4abc59eff13
Efi Kaltirimidou <efikalti@gmail.com>
2017-12-05 13:24:08 +0200

    project management with npm and node

---
+++ cli.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+Web3 = require('web3');
+
+console.log(Web3);
+
+web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+console.log(web3.eth.accounts);
+
+fs = require("fs");
+
+code = fs.readFileSync('CrowdfundedCharity.sol').toString();
+
+solc = require('solc');
+
+console.log(solc);
+
+compiledCode = solc.compile(code);
+
+abiDefinition = JSON.parse(compiledCode.contracts[':CrowdfundedCharity'].interface);
+CharityContract = web3.eth.contract(abiDefinition);
+
+byteCode = compiledCode.contracts[':CrowdfundedCharity'].bytecode;
+console.log(byteCode);
+
+deployedContract = CharityContract.new(['Rama','Nick','Jose'],{data: byteCode, from: web3.eth.accounts[0], gas: 4700000})
+
+console.log(deployedContract.address);
+
+contractInstance = CharityContract.at(deployedContract.address);
